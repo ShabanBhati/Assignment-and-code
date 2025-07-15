9560d1debddcd2f7dd1194a0c1b6e300add084b9
Shaban Bhati <iambhati77@gmail.com>
2025-07-15 12:06:30 +0530

    Create timer.js

---
+++ javascrpit/timer.js
@@ -0,0 +1,11 @@
+// let intervel = setInterval(()=>{
+//     console.log('hello')
+// },500)
+
+// clearInterval(intervel)
+
+let timeout = setTimeout(()=>{
+    console.log('hello')
+},2000)
+
+clearTimeout(timeout)\
